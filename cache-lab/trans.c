71dbeffdddbc6223f3940be541a4b169aca706aa
Rio <d530947429@gmail.com>
2023-07-19 11:08:35 +0800

    shell lab环境准备

--- cache-lab/trans.c
+++ cache-lab/trans.c
@@ -22,6 +22,48 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if(M==32) {
+        int i, j, k, v1, v2, v3, v4, v5, v6, v7, v8;
+		for (i = 0; i < 32; i += 8)
+			for(j = 0; j < 32; j += 8)
+				for(k = i; k < (i + 8); ++k)
+				{
+					v1 = A[k][j];
+					v2 = A[k][j+1];
+					v3 = A[k][j+2];
+					v4 = A[k][j+3];
+					v5 = A[k][j+4];
+					v6 = A[k][j+5];
+					v7 = A[k][j+6];			
+					v8 = A[k][j+7];
+					B[j][k] = v1;
+					B[j+1][k] = v2;
+					B[j+2][k] = v3;
+					B[j+3][k] = v4;
+					B[j+4][k] = v5;
+					B[j+5][k] = v6;
+					B[j+6][k] = v7;
+					B[j+7][k] = v8;
+				}
+    }
+    else  {
+        int i, j, k, v1, v2, v3, v4;
+		for (i = 0; i < 64; i += 4)
+			for(j = 0; j < 64; j += 4)
+				for(k = i; k < (i + 4); ++k)
+				{
+					v1 = A[k][j];
+					v2 = A[k][j+1];
+					v3 = A[k][j+2];
+					v4 = A[k][j+3];
+			
+					B[j][k] = v1;
+					B[j+1][k] = v2;
+					B[j+2][k] = v3;
+					B[j+3][k] = v4;
+				
+				}
+    }
 }
 
 /* 
@@ -35,14 +77,28 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 char trans_desc[] = "Simple row-wise scan transpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
+    int i, j, k, v1, v2, v3, v4, v5, v6, v7, v8;
+		for (i = 0; i < 64; i += 8)
+			for(j = 0; j < 64; j += 8)
+				for(k = i; k < (i + 8); ++k)
+				{
+					v1 = A[k][j];
+					v2 = A[k][j+1];
+					v3 = A[k][j+2];
+					v4 = A[k][j+3];
+					v5 = A[k][j+4];
+					v6 = A[k][j+5];
+					v7 = A[k][j+6];			
+					v8 = A[k][j+7];
+					B[j][k] = v1;
+					B[j+1][k] = v2;
+					B[j+2][k] = v3;
+					B[j+3][k] = v4;
+					B[j+4][k] = v5;
+					B[j+5][k] = v6;
+					B[j+6][k] = v7;
+					B[j+7][k] = v8;
+				}
 
 }
 
